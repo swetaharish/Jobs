5b39bfe84d4ab597ecd129a91e0726f5eb55b938
INHrawat <harish.rawat@symphonyteleca.com>
2016-01-27 13:10:19 +0530

    Code clean up

--- app/src/main/java/siatech/com/jobs/SplashActivity.java
+++ app/src/main/java/siatech/com/jobs/SplashActivity.java
@@ -1,13 +1,7 @@
 package siatech.com.jobs;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -17,27 +11,5 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         //Demo commit
        
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
